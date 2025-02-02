66b0e057a45a27fd4edd30de59f4afcab36ee34f
LambdaContinuum <azad.r.ellafi@gmail.com>
2025-02-02 16:38:43 -0500

    modified git

--- home-manager/features/git.nix
+++ home-manager/features/git.nix
@@ -5,5 +5,12 @@
     enable = true;
     userName = "LambdaContinuum";
     userEmail = "azad.r.ellafi@gmail.com";
+
+    extraConfig = {
+     # Sign all commits using ssh key
+        commit.gpgsign = true;
+        gpg.format = "ssh";
+        user.signingkey = "~/.ssh/id_ed25519.pub";
+      };
   };
 }
